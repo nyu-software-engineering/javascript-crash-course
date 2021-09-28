d79fba64780a675a69d3e76f3dba6c2a32d3f798
bloombar <amos@cs.nyu.edu>
2021-09-28 16:42:46 -0400

    clarifications

--- 4.objects/2.object_methods_standard.js
+++ 4.objects/2.object_methods_standard.js
@@ -3,6 +3,7 @@ const fido = {
   breed: "Schnauzer",
   // an instance method, written in standard function definition style
   bark(message) {
+    // the 'this' keyword refers to the object to which this method belongs
     console.log(`${this.name} the ${this.breed} says, '${message}'`)
   },
 }
--- 4.objects/5.object_inheritance_prototypal.js
+++ 4.objects/5.object_inheritance_prototypal.js
@@ -16,3 +16,19 @@ robotMe.isRobot = true // modify this new object in some way
 
 console.log(`me ${me.isRobot ? "is indeed" : "is not"} a robot.`) // expected output: "My name is Foo Barstein"
 console.log(`robotMe ${robotMe.isRobot ? "is indeed" : "is not"} a robot.`) // expected output: "My name is Foo Barstein"
+
+// add human speech abilities to the human object
+me.speak = function (message) {
+  console.log(`${this.name} the human says, "${message}".`)
+}
+
+// add robot-specific speech abilities to the robot object
+robotMe.speak = function (message) {
+  console.log(
+    `${this.name} the robot states in monotonous voice, "${message}".`
+  )
+}
+
+// let each speek
+me.speak("Hello!")
+robotMe.speak("Hello!")
